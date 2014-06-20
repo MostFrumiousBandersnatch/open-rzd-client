b2f63ab56c64bc3c74f9cc77c6fcfa826038ab19
Ivan Kondratyev <ivanbright@gmail.com>
2014-06-20 16:28:48 +0400

    Test suite for encodeDict.

--- test/test.js
+++ test/test.js
@@ -8,11 +8,60 @@ describe('RZD open API client', function () {
         module('rzd');
     });
 
-    describe('encodeDict defined', function () {
-        it('should be a working service', inject(['encodeDict',
-                function (encodeDict) {
-                    expect(encodeDict).toBeDefined();
-                }
-        ]));
+    describe('encodeDict', function () {
+        var encodeDictFactory;
+
+        beforeEach(inject(
+            function (encodeDict) {
+                encodeDictFactory = encodeDict;
+            }
+        ));
+
+        it('should exists',
+            function () {
+                expect(encodeDictFactory).toBeDefined();
+            }
+        );
+
+        it('should performs serialization',
+            function () {
+                var source = {
+                        a: 1,
+                        b: 'M&Ms'
+                    },
+                    serialized = 'a=1&b=M%26Ms';
+
+                expect(encodeDictFactory(source)).toEqual(serialized);
+            }
+        );
+
+        it('should performs serialization without encoding as well',
+            function () {
+                var source = {
+                        a: 'привет',
+                        b: 'пока'
+                    },
+                    serialized = 'a=привет&b=пока';
+
+                expect(encodeDictFactory(source, true)).toEqual(serialized);
+            }
+        );
+
+        it('should skip empty items in case',
+            function () {
+                var source = {
+                        a: 1,
+                        b: '',
+                        c: 3
+                    },
+                    fully_serialized = 'a=1&b=&c=3',
+                    mostly_serialized = 'a=1&c=3';;
+
+                expect(encodeDictFactory(source)).toEqual(fully_serialized);
+                expect(encodeDictFactory(source, false, true)).toEqual(
+                    mostly_serialized
+                );
+            }
+        );
     });
 });
